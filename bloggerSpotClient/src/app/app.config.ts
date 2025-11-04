790634936911e0e06619f1c9b5c42cfaea63392c
nishchay <ramaulnishchay@gmail.com>
2025-11-04 17:44:50 +0530

    jwt config

---
+++ bloggerSpotClient/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './services/auth-interceptor/auth.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    HttpClient,
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
+};
